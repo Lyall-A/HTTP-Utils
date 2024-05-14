43d33112259821c14e66155de637bd8179221978
Lyall <lyall.yeet@gmail.com>
2024-05-14 15:08:30 +0100

    aaa

---
+++ App.js
@@ -0,0 +1,25 @@
+const Router = require("./Router");
+const Server = require("./Server");
+
+module.exports = class {
+    constructor() {
+        this.router = new Router();
+        this.server = new Server({ route: this.router.route });
+
+        this.on = (...args) => this.server.on(...args);
+        this.once = (...args) => this.server.once(...args);
+    
+        this.listen = (...args) => this.server.listen(...args);
+    
+        this.get = (...args) => this.router.get(parseServerObj(...args));
+        this.head = (...args) => this.router.head(parseServerObj(...args));
+        this.post = (...args) => this.router.post(parseServerObj(...args));
+        this.put = (...args) => this.router.put(parseServerObj(...args));
+        this.delete = (...args) => this.router.delete(parseServerObj(...args));
+    }
+}
+
+function parseServerObj(req, res) {
+    const newReq = { _raw: req };
+    const newRes = { _raw: res };
+}
